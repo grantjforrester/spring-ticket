e9a309c6a917096b06dee961deb26cf2ac69bff5
Grant Forrester <grantforrester@gmail.com>
2024-07-18 16:48:10 +0100

    refactor: renamed main application class

--- src/main/java/com/github/grantjforrester/springticket/Application.java
+++ src/main/java/com/github/grantjforrester/springticket/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringTicketApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringTicketApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
