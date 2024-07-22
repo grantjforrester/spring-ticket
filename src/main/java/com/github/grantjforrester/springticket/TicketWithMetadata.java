d7c52b2818a3a8decfdd267dc506c6928ef194cf
Grant Forrester <grantforrester@gmail.com>
2024-07-22 18:17:39 +0100

    chore: formatting

--- src/main/java/com/github/grantjforrester/springticket/TicketWithMetadata.java
+++ src/main/java/com/github/grantjforrester/springticket/TicketWithMetadata.java
@@ -5,9 +5,9 @@ import org.hibernate.validator.constraints.UUID;
 import jakarta.validation.constraints.NotBlank;
 
 public record TicketWithMetadata(
-        @NotBlank(message = "missing field:", groups = Metadata.class) @UUID String id,
-        @NotBlank(message = "missing field:", groups = Metadata.class) String version,
-        @NotBlank(message = "missing field:", groups = Model.class) String summary,
-        String description,
-        @NotBlank(message = "missing field:", groups = Model.class) String status) {
+                @NotBlank(message = "missing field:", groups = Metadata.class) @UUID String id,
+                @NotBlank(message = "missing field:", groups = Metadata.class) String version,
+                @NotBlank(message = "missing field:", groups = Model.class) String summary,
+                String description,
+                @NotBlank(message = "missing field:", groups = Model.class) String status) {
 }
