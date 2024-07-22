02200f4aa88f5a4a909dd4e88a7735b3601dae5d
Grant Forrester <grantforrester@gmail.com>
2024-07-22 18:17:08 +0100

    feat: add health check endpoint

--- src/main/java/com/github/grantjforrester/springticket/Api.java
+++ src/main/java/com/github/grantjforrester/springticket/Api.java
@@ -4,8 +4,10 @@ import java.io.IOException;
 import java.io.InputStream;
 
 import org.apache.commons.io.IOUtils;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,6 +18,11 @@ public final class Api {
         InputStream in = getClass()
                 .getResourceAsStream("/openapi.yml");
         return IOUtils.toByteArray(in);
+    }
+
+    @GetMapping("/health")
+    @ResponseStatus(HttpStatus.OK)
+    void health() {
     }
 
 }
